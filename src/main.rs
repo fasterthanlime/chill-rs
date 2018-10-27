f9b5e04019b3a40645c420891a6e9424156176b1
Amos Wenger <amoswenger@gmail.com>
2018-10-27 22:57:36 +0200

    streaming stuff!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,54 @@
 extern crate reqwest;
 
+use std::io::{self, Write};
+use std::time::SystemTime;
+
 fn main() -> Result<(), reqwest::Error> {
     // let url = "https://itch.io/country";
     // let url = "http://neverssl.com/";
-    let url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://itch.io/country";
+    let url = "http://localhost:6666";
+
+    let start = SystemTime::now();
+    let now = || format!("{:?}\t", start.elapsed().unwrap());
 
     let client = reqwest::Client::new();
+    println!("{} get()...", now());
     let req = client.get(url);
-    println!("Called get()...");
 
+    println!("{} send()...", now());
     let mut res = req.send()?;
 
-    println!("Called send...");
+    println!("{} reading headers", now());
     if let Some(loc) = res.headers().get("content-type") {
         if let Ok(loc) = loc.to_str() {
             println!("Location: {}", loc);
         }
     }
 
-    println!("Parsing as text...");
-    println!("{}", res.text()?);
+    println!("{} streaming body...", now());
+    let mut sink = Discard { count: 0 };
+    res.copy_to(&mut sink)?;
+    println!("{} done streaming {} bytes", now(), sink.count());
+
     Ok(())
+}
+
+struct Discard {
+    count: usize,
+}
+
+impl Discard {
+    pub fn count(&self) -> usize {
+        self.count
+    }
+}
+
+impl Write for Discard {
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        self.count += buf.len();
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> Result<(), io::Error> {
+        Ok(())
+    }
 }
